31a4eb3f862543c28acacbc96df8c71e01be4ace
enthusiastic-mouse-1767 <ebisiemen@gmail.com>
2019-01-27 23:30:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,4 +1,11 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+  end 
+end  
+  
+  
+  # code an argument here
   # Your code here
 end
 
